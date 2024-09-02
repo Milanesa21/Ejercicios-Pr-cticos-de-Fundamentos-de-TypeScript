78abb27a4bec8575a6e1f59765ee06345ad4d64e
Milanesa 21 <diegojara88574@gmail.com>
2024-09-02 11:18:53 -0300

    Se realizo la actividad

--- src/tarea15.ts
+++ src/tarea15.ts
@@ -4,6 +4,6 @@ function idenity<T>(valor: T): T {
 
 console.log(idenity<string>("Mi nombre es Kira Yoshikage, tengo"));
 
-console.log(idenity<Number>(33));
+console.log(idenity<number>(33));
 
-console.log(idenity<string>("años y mi sueño es vivir tranquilo"));
+console.log(idenity<string>("años. Mi casa está en la sección noreste de Morioh, donde están todas villas, y no estoy casado. Trabajo como empleado de los grandes almacenes Kame Yu, y llego a casa todos los días a más tardar 8 p.m.. No fumo, pero ocasionalmente bebo. Estoy en la cama a las 11 de la noche y me aseguro de dormir ocho horas, pase lo que pase. Después de tomar un vaso de leche tibia y hacer unos veinte minutos de estiramientos antes de acostarme, generalmente no tengo problemas para dormir hasta la mañana. Al igual que un bebé, me levanto sin fatiga ni estrés por la mañana. Me dijeron que no había problemas en mi último chequeo. Estoy tratando de explicar que soy una persona que desea vivir una vida muy tranquila. Tengo cuidado de no molestarme con ningún enemigo, como ganar o perder, que me haría perder el sueño por la noche. Así es como trato con la sociedad, y sé que eso es lo que me trae felicidad. Aunque, si tuviera que pelear, no perdería con nadie. "));
